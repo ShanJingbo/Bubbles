8602dfd73d8b06ef052d57fcb38524feaacbe7da
Shan Jingbo <138432448+ShanJingbo@users.noreply.github.com>
2023-12-02 16:49:35 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='Bubbles',
+    version='0.1.0',
+    keywords='yhz-bubble',
+    author='Shan Jingbo',
+    author_email='equinolstice@outlook.com',
+    description='a library for joy',
+    long_description="ShareClass",
+    long_description_content_type="ShareClass",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    platforms='any',
+    install_requires=[],
+)
+
